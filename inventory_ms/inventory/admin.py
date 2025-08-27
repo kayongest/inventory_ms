83d108fbc58038f3306bc606c15de4c922ae2027
kayongest <kayonga70@gmail.com>
2025-08-27 11:57:49 +0200

    Setting up the admin interface

--- inventory_ms/inventory/admin.py
+++ inventory_ms/inventory/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import InventoryItem, Category, InventoryChange
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+@admin.register(InventoryItem)
+class InventoryItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'quantity', 'price', 'created_by', 'date_added']
+    list_filter = ['category', 'date_added']
+    search_fields = ['name', 'description']
+    readonly_fields = ['date_added', 'last_updated']
+
+@admin.register(InventoryChange)
+class InventoryChangeAdmin(admin.ModelAdmin):
+    list_display = ['item', 'change_type', 'user', 'previous_quantity', 'new_quantity', 'timestamp']
+    list_filter = ['change_type', 'timestamp']
+    readonly_fields = ['item', 'user', 'change_type', 'previous_quantity', 'new_quantity', 'change_amount', 'timestamp', 'notes']
+    
+    def has_add_permission(self, request):
+        return False
