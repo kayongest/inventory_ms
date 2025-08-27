5e40d8afd35c6c2b4a779994191ccd605c36fd8d
kayongest <kayonga70@gmail.com>
2025-08-27 11:44:20 +0200

    Setting up urls

---
+++ inventory_ms/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'categories', views.CategoryViewSet)
+router.register(r'items', views.InventoryItemViewSet)
+router.register(r'changes', views.InventoryChangeViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+]
